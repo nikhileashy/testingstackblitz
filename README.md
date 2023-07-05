7d5db26385668385ca7178cd94f2b83d03d71d14
nikhileashy <70377480+nikhileashy@users.noreply.github.com>
2023-07-05 14:33:45 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# testingstackblitz
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-starters-k2epux)
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+import { Fade,Slide } from "react-awesome-reveal";
+
+export default function App() {
+
+  return (
+    <div>
+      <Fade><h1>Hello StackBlitz!</h1></Fade>
+      <Slide><h1>Hello StackBlitz!</h1></Slide>
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
+}
